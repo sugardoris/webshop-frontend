88be8af4e8437f9aa3570aaf0bdf0a1513667ca9
Doris Šugar <sugardoris1@yahoo.com>
2022-01-29 17:38:04 +0100

    Login and register validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,22 +14,75 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  submitted: boolean = false;
+  submittedLogin: boolean = false;
+  submitterRegister: boolean = false;
+  loginMode: boolean = true;
 
   loginFormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
   });
 
-  get username() {
-    return this.loginFormGroup.get('username');
+  get email() {
+    return this.loginFormGroup.get('email');
   }
 
   get password() {
     return this.loginFormGroup.get('password');
   }
 
+  matchPasswordValidator(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const pass = control.parent?.get(matchTo);
+      return control.value === pass?.value ? null : { notSame: true };
+    };
+  }
+
+  registerFormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', [
+      Validators.required,
+      this.matchPasswordValidator('password'),
+    ]),
+  });
+
+  get registerEmail() {
+    return this.registerFormGroup.get('email');
+  }
+
+  get registerPassword() {
+    return this.registerFormGroup.get('password');
+  }
+
+  get repeatPassword() {
+    return this.registerFormGroup.get('repeatPassword');
+  }
+
   onSubmit(): void {
-    this.submitted = true;
+    if (this.loginMode) {
+      this.submittedLogin = true;
+      let user = {
+        email: this.loginFormGroup.value.email,
+        password: this.loginFormGroup.value.password,
+      };
+      console.log(user);
+    } else {
+      this.submitterRegister = true;
+      let user = {
+        email: this.registerFormGroup.value.email,
+        password: this.registerFormGroup.value.password,
+        role: 'user',
+      };
+      console.log(user);
+    }
+  }
+
+  registerMode() {
+    this.loginMode = !this.loginMode;
+  }
+
+  onRegister() {
+    console.log(this.registerFormGroup.value);
   }
 }
